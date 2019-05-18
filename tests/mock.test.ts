76bcceb75acc73cc0dce9bc248573a85bcf397f1
Ly Lam <llam@fairventureslab.ca>
2019-05-18 01:11:22 -0400

    Setup tests

---
+++ tests/mock.test.ts
@@ -0,0 +1,10 @@
+//import {MockConfigurationStore} from '../index'
+
+describe('mock config store', () => {
+ //   const store = new MockConfigurationStore();
+
+test('set value', () => {
+    
+})
+
+});
