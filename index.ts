215181e507d6a5bfc081a1993eb9f96255ad3a64
Scott Morris <smorris@fairventureslab.ca>
2019-05-21 13:19:34 -0400

    Reformat with new Prettier settings.

--- index.ts
+++ index.ts
@@ -1,67 +1,67 @@
 export interface IConfigurationStore {
-  setGlobalData<T>(key: string, value: T): Promise<T>
-  getGlobalData<T>(key: string, defaultValue?: T): Promise<T>
-  updateGlobalData<T>(key: string, value: T): Promise<T>
-  setUserData<T>(key: string, value: T): Promise<T>
-  getUserData<T>(key: string, defaultValue?: T): Promise<T>
-  updateUserData<T>(key: string, value: T): Promise<T>
+	setGlobalData<T>(key: string, value: T): Promise<T>;
+	getGlobalData<T>(key: string, defaultValue?: T): Promise<T>;
+	updateGlobalData<T>(key: string, value: T): Promise<T>;
+	setUserData<T>(key: string, value: T): Promise<T>;
+	getUserData<T>(key: string, defaultValue?: T): Promise<T>;
+	updateUserData<T>(key: string, value: T): Promise<T>;
 }
 
 export abstract class BaseConfigurationStore implements IConfigurationStore {
-  constructor(
-    public userID: string,
-    private globalPath = "internal/global/",
-    private userPath = "internal/user/"
-  ) {}
+	constructor(
+		public userID: string,
+		private globalPath = 'internal/global/',
+		private userPath = 'internal/user/'
+	) {}
 
-  protected abstract setData<T>(settingsPath: string, value: T): Promise<T>
-  protected abstract getData<T>(settingsPath: string, defaultValue?: T): Promise<T>
-  protected abstract updateData<T>(settingsPath: string, value: T): Promise<T>
+	protected abstract setData<T>(settingsPath: string, value: T): Promise<T>;
+	protected abstract getData<T>(settingsPath: string, defaultValue?: T): Promise<T>;
+	protected abstract updateData<T>(settingsPath: string, value: T): Promise<T>;
 
-  private getGlobalSettingsPath(key: string): string {
-    return `${this.globalPath}${key}`
-  }
+	private getGlobalSettingsPath(key: string): string {
+		return `${this.globalPath}${key}`;
+	}
 
-  private getUserSettingsPath(key: string): string {
-    return `${this.userPath}${this.userID}/${key}`
-  }
+	private getUserSettingsPath(key: string): string {
+		return `${this.userPath}${this.userID}/${key}`;
+	}
 
-  setGlobalData<T>(key: string, value: T): Promise<T> {
-    return this.setData<T>(this.getGlobalSettingsPath(key), value)
-  }
+	setGlobalData<T>(key: string, value: T): Promise<T> {
+		return this.setData<T>(this.getGlobalSettingsPath(key), value);
+	}
 
-  getGlobalData<T>(key: string, defaultValue?: T): Promise<T> {
-    return this.getData<T>(this.getGlobalSettingsPath(key), defaultValue)
-  }
+	getGlobalData<T>(key: string, defaultValue?: T): Promise<T> {
+		return this.getData<T>(this.getGlobalSettingsPath(key), defaultValue);
+	}
 
-  updateGlobalData<T>(key: string, value: T): Promise<T> {
-    return this.updateData<T>(this.getGlobalSettingsPath(key), value)
-  }
+	updateGlobalData<T>(key: string, value: T): Promise<T> {
+		return this.updateData<T>(this.getGlobalSettingsPath(key), value);
+	}
 
-  setUserData<T>(key: string, value: T): Promise<T> {
-    return this.setData<T>(this.getUserSettingsPath(key), value)
-  }
+	setUserData<T>(key: string, value: T): Promise<T> {
+		return this.setData<T>(this.getUserSettingsPath(key), value);
+	}
 
-  getUserData<T>(key: string, defaultValue?: T): Promise<T> {
-    return this.getData<T>(this.getUserSettingsPath(key), defaultValue)
-  }
+	getUserData<T>(key: string, defaultValue?: T): Promise<T> {
+		return this.getData<T>(this.getUserSettingsPath(key), defaultValue);
+	}
 
-  updateUserData<T>(key: string, value: T): Promise<T> {
-    return this.updateData<T>(this.getUserSettingsPath(key), value)
-  }
+	updateUserData<T>(key: string, value: T): Promise<T> {
+		return this.updateData<T>(this.getUserSettingsPath(key), value);
+	}
 }
 
 export class MockConfigurationStore extends BaseConfigurationStore {
-  private data: { [id: string]: any } = {}
+	private data: { [id: string]: any } = {};
 
-  protected async setData<T>(settingsPath: string, value: T): Promise<T> {
-    this.data[settingsPath] = value
-    return value
-  }
-  protected async getData<T>(settingsPath: string, defaultValue?: T): Promise<T> {
-    return this.data[settingsPath] || defaultValue
-  }
-  protected updateData<T>(settingsPath: string, value: T): Promise<T> {
-    return this.setData(settingsPath, value)
-  }
+	protected async setData<T>(settingsPath: string, value: T): Promise<T> {
+		this.data[settingsPath] = value;
+		return value;
+	}
+	protected async getData<T>(settingsPath: string, defaultValue?: T): Promise<T> {
+		return this.data[settingsPath] || defaultValue;
+	}
+	protected updateData<T>(settingsPath: string, value: T): Promise<T> {
+		return this.setData(settingsPath, value);
+	}
 }
